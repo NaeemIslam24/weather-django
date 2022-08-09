4604b5fbb77677ab7a919f159239bb10a3f9e7ad
nayem <naeemislam13790@gmail.com>
2022-08-10 00:36:43 +0600

    'commited'

--- weathers/weather/views.py
+++ weathers/weather/views.py
@@ -37,6 +37,3 @@ def index(request):
 
     context = {'weather_data' : weather_data, 'form' : form}
     return render(request, 'weather/weather.html', context)
-
-
-{'coord': {'lon': 91.1105, 'lat': 23.9691}, 'weather': [{'id': 721, 'main': 'Haze', 'description': 'haze', 'icon': '50d'}], 'base': 'stations', 'main': {'temp': 91.47, 'feels_like': 104.07, 'temp_min': 91.47, 'temp_max': 91.47, 'pressure': 1003, 'humidity': 70}, 'visibility': 4500, 'wind': {'speed': 0, 'deg': 0}, 'clouds': {'all': 75}, 'dt': 1659258464, 'sys': {'type': 1, 'id': 9107, 'country': 'BD', 'sunrise': 1659223446, 'sunset': 1659271179}, 'timezone': 21600, 'id': 1185263, 'name': 'Brahmanbaria', 'cod': 200}
